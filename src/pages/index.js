3158f7365219216c5916490bd94632c140f4bda7
Dorjsuren <dddqwerty@Dorjsurens-MacBook-Pro.local>
2022-04-27 18:54:43 +0800

    added icons handler

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import { Hello } from 'components'
+import { handleIcon } from 'utils'
 
 export default function Home() {
   return (
     <div>
+      {handleIcon({ icon: 'instagram', className: 'bg-amber-200' })}
       <Hello />
     </div>
   )
