3d0a279b4dec9f571c94d74a8b1c2978282adee0
UnknownOne76 <ganboldgantor14@gmail.com>
2022-04-30 11:43:10 +0800

    changed style of colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -3,15 +3,12 @@ module.exports = {
     light: '#A9CF78',
     main: '#9AC760',
     dark: '#8BBF48',
+    black: '#0F233E',
+    white: '#FFFFFF',
   },
-  secondary: {
-    light: '#2B496B',
-    main: '#274260',
-    dark: '#233B55',
-  },
-  black: {
-    light: '#0F233E',
-    main: '#959DA7',
-    dark: '#ECF3FC',
+  grey: {
+    g100: '#404040',
+    g200: '#C4C4C4',
+    g300: '#F5FAFF',
   },
 }
