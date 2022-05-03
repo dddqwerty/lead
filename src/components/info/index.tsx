fec6fe7bba4c95059cc5665c5b00a473be1b8d49
Kali-Linux <ganboldgantor14@gmail.com>
2022-05-04 06:33:15 +0800

    detailed

--- src/components/info/index.tsx
+++ src/components/info/index.tsx
@@ -1,8 +1,15 @@
 import Typo from "components/typography";
 import { useEffect } from "react";
+import { ReactNode } from "react";
 import { useState } from "react";
 import { ReactElement } from "react";
 import { FC } from "react";
+
+type Props = {
+    children: React.ReactNode,
+    onClick?: () => void,
+    name: string,
+}
 
 
 export const Information: FC = (): ReactElement => {
@@ -13,6 +20,19 @@ export const Information: FC = (): ReactElement => {
     const [cont , setCont] = useState<boolean>(false); 
     const [read , setRead ] = useState<boolean>(true); 
     const [data , setData] = useState<any>(); 
+
+    const DetailedInfo = (props: Props) => {
+
+        const { onClick , children, name } = props;
+        
+        const getClass = (props: string) => {
+            switch(props) {
+                case 'info': return 'p-2 border-b-2 hover:border-l-4 border-primary-main'
+            }
+        }
+
+        return <div className={`${getClass(name)}`} onClick={onClick}> {children} </div>
+    }; 
 
     const infos = {
         tailbar: {
@@ -80,13 +100,13 @@ export const Information: FC = (): ReactElement => {
        <div className='flex flex-row justify-items-center w-full m-5 p-10'>
        <Typo variant="title1" className="mt-1"> {data && data.title} </Typo>
            <div className="flex flex-col justify-center border-t-2 border-[#0F233E] border-opacity-10 w-[40%] m-3.5 p-2"> 
-           <Typo variant="body" className="leading-7"> {data && data.info && read ? data.info.slice(0 , 190) : data && data.info ? data.info : 'ret'} <Typo variant="body" className="flex flex-row text-primary-main cursor-pointer" onClick={toggleRead}> {read ? 'Дэлгэрэнгүй' : 'Буцаах'} <img src="/static/arrow.png" alt="" className="w-6 h-6 mt-1"/> </Typo> </Typo> 
+           <Typo variant="body" className="leading-7 mt-5"> {data && data.info && read ? data.info.slice(0 , 190) : data && data.info ? data.info : 'ret'} <Typo variant="body" className="flex flex-row text-primary-main cursor-pointer" onClick={toggleRead}> {read ? 'Дэлгэрэнгүй' : 'Буцаах'} <img src="/static/arrow.png" alt="" className="w-6 h-6 mt-1"/> </Typo> </Typo> 
            </div>
            <div className="flex flex-col w-72 h-2/4 bg-[#F7F7F7]">
-           <div onClick={option1} className='p-2 border-b-2 hover:border-l-4 border-primary-main'> <Typo variant="body" className="pointer-events-none"> Тайлбар </Typo> </div>
-           <div onClick={option2} className='p-2 border-b-2 hover:border-l-4 border-primary-main'> <Typo variant="body" className="pointer-events-none"> Зорилго </Typo> </div>
-           <div onClick={option3} className='p-2 border-b-2 hover:border-l-4 border-primary-main'> <Typo variant="body" className="pointer-events-none"> Хийсэн зүйлс </Typo> </div>
-           <div onClick={option4} className='p-2 border-b-2 hover:border-l-4 border-primary-main'> <Typo variant="body" className="pointer-events-none"> Үргэлжлэл бий </Typo> </div>
+           <DetailedInfo name="info" onClick={option1}> <Typo variant="body" className="pointer-events-none"> Тайлбар </Typo> </DetailedInfo>
+           <DetailedInfo name='info' onClick={option2}> <Typo variant="body" className="pointer-events-none"> Зорилго </Typo> </DetailedInfo>
+           <DetailedInfo name="info" onClick={option3}> <Typo variant="body" className="pointer-events-none"> Хийсэн зүйлс </Typo> </DetailedInfo>
+           <DetailedInfo name="info" onClick={option4}> <Typo variant="body" className="pointer-events-none"> Үргэлжлэл бий </Typo> </DetailedInfo>
            </div>
         </div>
         </>
