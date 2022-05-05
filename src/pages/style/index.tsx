681b2fefc59b8e89587b4967f5d47ad66e17b549
UnknownOne76 <ganboldgantor14@gmail.com>
2022-05-05 21:42:54 +0800

    accompany

--- src/pages/style/index.tsx
+++ src/pages/style/index.tsx
@@ -21,6 +21,13 @@ export default function In() {
           </Typography>
         </a>
       </Link>
+      <Link href={'/style/accom'}>
+        <a>
+          <Typography variant="body-bold" className="text-primary-black hover:underline">
+            Go to the accompany page
+          </Typography>
+        </a>
+      </Link>
       <div className='flex gap-2'>
         <Button variant='fill'>
           <Typography variant='body' className='text-white'>
