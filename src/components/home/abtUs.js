c1f02d99db7dd382394341d62cb38060570bdb22
bekkung <smileever.b329@gmail.com>
2022-05-16 18:37:27 +0800

    imrp

---
+++ src/components/home/abtUs.js
@@ -0,0 +1,28 @@
+import Typo from 'components/typography'
+
+export const StartUp = () => {
+  return (
+    <div className="flex flex-col justify-center items-center mt-[84px] p-[8px] mb-[173.3px]">
+      <Typo variant="body-semibold" className="text-primary-main">
+        {' '}
+        Бидний тухай{' '}
+      </Typo>
+      <Typo variant="h2" className="text-[#0F233E]">
+        {' '}
+        НЭГ ГЭР БҮЛ{' '}
+      </Typo>
+      <div className="flex justify-center items-center flex-col">
+        <Typo variant="body" className="text-[#0F233E] flex justify-center">
+          {' '}
+          Иргэн бүрийг тэгш хүртээмжтэй{' '}
+        </Typo>
+        <Typo variant="body" className="text-[#0F233E] flex justify-center">
+          {' '}
+          нийгмийг цогцлооход нэгдсэн нэг гэр бүл{' '}
+        </Typo>
+      </div>
+      <img src="/static/leadie.svg" alt="" width={722} height={188} className=" mt-[50px]" />
+    </div>
+  )
+}
+export default StartUp
