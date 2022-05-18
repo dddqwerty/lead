60efb2bffe7071d26d12286d683cf01b5ede5011
Ectrolad <ectrolad@Nest-100s-iMac.local>
2022-05-18 19:01:01 +0800

    home second

---
+++ src/components/home/medee.js
@@ -0,0 +1,12 @@
+import { Typography } from "components";
+import Typo from "components/typography";
+
+export const Medee = () => {
+    return (
+        <div className="flex flex-col gap-8">
+            <Typography variant="h3" className="flex text-primary-dark" > 28 Jun 21</Typography>
+            <Typography variant="h1" className="flex ">Сонгуулийн ирцийн мэдээ ба технологийн шийдэл.</Typography>
+        </div>
+    )
+}
+export default Medee;
