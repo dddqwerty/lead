6ac5511854eedee0ec47db7dffe75fc4117686e3
bekkung <smileever.b329@gmail.com>
2022-05-18 17:32:49 +0800

    about us

--- src/components/wks/index.tsx
+++ src/components/wks/index.tsx
@@ -1,113 +1,112 @@
 import CustomButton from "components/button";
 import Typo from "components/typography";
-import { MainLayout } from "layout";
 import { toUpper } from "lodash";
 import { useEffect } from "react";
 import { useState } from "react";
 
 const obj = [
-    ['Dulguun' , 'chairman' , 'Img.svg'],  
-    ['E. Dulguun' , 'Hackerman' , 'Img.svg'], 
-    ['noName' , 'Officer' , 'Img.svg'],
-    ['noName' , 'Broker' , 'Img.svg'],
-    ['noName' , 'noType' , 'Img.svg'],
-    ['noName' , 'noType' , 'Img.svg'],
-    ['noName' , 'Racer' , 'Img.svg'],
-    ['noName' , 'Dropping' , 'Img.svg'],
-    ['noName' , 'Bomber' , 'Img.svg'],
-    ['noName' , 'Hmm' , 'Img.svg'],
-    ['noName' , 'Idk' , 'Img.svg'],
-    ['noName' , 'Idek' , 'Img.svg'],
-]; 
+    ['Dulguun', 'chairman', 'Img.svg'],
+    ['E. Dulguun', 'Hackerman', 'Img.svg'],
+    ['noName', 'Officer', 'Img.svg'],
+    ['noName', 'Broker', 'Img.svg'],
+    ['noName', 'noType', 'Img.svg'],
+    ['noName', 'noType', 'Img.svg'],
+    ['noName', 'Racer', 'Img.svg'],
+    ['noName', 'Dropping', 'Img.svg'],
+    ['noName', 'Bomber', 'Img.svg'],
+    ['noName', 'Hmm', 'Img.svg'],
+    ['noName', 'Idk', 'Img.svg'],
+    ['noName', 'Idek', 'Img.svg'],
+];
 
 const obj1 = [
-    ['Bryan' , 'Manager' , 'img1.svg'],  
-    ['Unknown' , 'HackerMan' , 'img1.svg'], 
-    ['noName' , 'Officer' , 'Img.svg'],
-    ['noName' , 'Broker' , 'Img.svg'],
-    ['noName' , 'noType' , 'Img.svg'],
-    ['noName' , 'noType' , 'Img.svg'],
-    ['noName' , 'Racer' , 'Img.svg'],
-    ['noName' , 'Dropping' , 'Img.svg'],
-    ['noName' , 'Bomber' , 'Img.svg'],
-    ['noName' , 'Hmm' , 'Img.svg'],
-    ['noName' , 'Idk' , 'Img.svg'],
-    ['noName' , 'Idek' , 'Img.svg'],
+    ['Bryan', 'Manager', 'img1.svg'],
+    ['Unknown', 'HackerMan', 'img1.svg'],
+    ['noName', 'Officer', 'Img.svg'],
+    ['noName', 'Broker', 'Img.svg'],
+    ['noName', 'noType', 'Img.svg'],
+    ['noName', 'noType', 'Img.svg'],
+    ['noName', 'Racer', 'Img.svg'],
+    ['noName', 'Dropping', 'Img.svg'],
+    ['noName', 'Bomber', 'Img.svg'],
+    ['noName', 'Hmm', 'Img.svg'],
+    ['noName', 'Idk', 'Img.svg'],
+    ['noName', 'Idek', 'Img.svg'],
 ];
 
 const obj2 = [
-    ['Dulguun' , 'CHAIRMAN' , 'Img.svg'],  
-    ['Unknown' , 'HackerMan' , 'Img.svg'], 
-    ['noName' , 'Officer' , 'Img.svg'],
-    ['noName' , 'Broker' , 'Img.svg'],
-    ['noName' , 'noType' , 'Img.svg'],
-    ['noName' , 'noType' , 'Img.svg'],
-    ['noName' , 'Racer' , 'Img.svg'],
-    ['noName' , 'Dropping' , 'Img.svg'],
-    ['noName' , 'Bomber' , 'Img.svg'],
-    ['noName' , 'Hmm' , 'Img.svg'],
-    ['noName' , 'Idk' , 'Img.svg'],
-    ['noName' , 'Idek' , 'Img.svg'],
+    ['Dulguun', 'CHAIRMAN', 'Img.svg'],
+    ['Unknown', 'HackerMan', 'Img.svg'],
+    ['noName', 'Officer', 'Img.svg'],
+    ['noName', 'Broker', 'Img.svg'],
+    ['noName', 'noType', 'Img.svg'],
+    ['noName', 'noType', 'Img.svg'],
+    ['noName', 'Racer', 'Img.svg'],
+    ['noName', 'Dropping', 'Img.svg'],
+    ['noName', 'Bomber', 'Img.svg'],
+    ['noName', 'Hmm', 'Img.svg'],
+    ['noName', 'Idk', 'Img.svg'],
+    ['noName', 'Idek', 'Img.svg'],
 ];
 
 
 export const Workers = () => {
 
-    const [data , setData] = useState<any>(obj); 
-    const [def , setDef] = useState<boolean>(true);
-    const [def1 , setDef1] = useState<boolean>(false); 
-    const [def2, setDef2] = useState<boolean>(false); 
-    const [toggle, setToggle] = useState<any>(null); 
+    const [data, setData] = useState<any>(obj);
+    const [def, setDef] = useState<boolean>(true);
+    const [def1, setDef1] = useState<boolean>(false);
+    const [def2, setDef2] = useState<boolean>(false);
+    const [toggle, setToggle] = useState<any>(null);
 
     useEffect(() => {
-        console.log(toggle); 
-    } , [toggle]); 
+        console.log(toggle);
+    }, [toggle]);
 
     const option = () => {
-        setData(obj); 
-        setDef(true); 
-        setDef1(false); 
-        setDef2(false); 
+        setData(obj);
+        setDef(true);
+        setDef1(false);
+        setDef2(false);
     }
 
     const option1 = () => {
-        setData(obj1); 
+        setData(obj1);
         setDef(false);
-        setDef1(true); 
-        setDef2(false); 
+        setDef1(true);
+        setDef2(false);
     }
 
     const option2 = () => {
-        setData(obj2); 
-        setDef(false); 
-        setDef1(false); 
-        setDef2(true); 
+        setData(obj2);
+        setDef(false);
+        setDef1(false);
+        setDef2(true);
     }
     return (
-        <> 
-        <div className="flex flex-row justify-start w-full">
-        <Typo variant="h3"> Удирдлагууд </Typo> 
-        <div className="flex flex-row justify-end w-full"> 
-        <CustomButton variant="text" classname={def ? `text-white hover:text-white bg-primary-main hover:bg-primary-main border-2`: 'text-black hover:text-white border-2  bg-[#F7F7F7] hover:bg-primary-main'} onClick={option}> 2021-2022 он </CustomButton>
-        <CustomButton variant="text" classname={def1 ? `text-white hover:text-white bg-primary-main hover:bg-primary-main border-2`: 'text-black hover:text-white border-2  bg-[#F7F7F7] hover:bg-primary-main'} onClick={option1}> 2019-2020 он </CustomButton>
-        <CustomButton variant="text" classname={def2 ? `text-white hover:text-white bg-primary-main hover:bg-primary-main border-2`: 'text-black hover:text-white border-2  bg-[#F7F7F7] hover:bg-primary-main'} onClick={option2}> 2018-2019 он </CustomButton>
-        </div> 
-        </div>
-        <div className="grid grid-rows-2 grid-flow-col gap-6 justify-start my-5"> 
-        {data.map((x: any , i: number) => {
-           return <div key={i}> 
-           <div className="flex flex-col justify-end w-full bg-gradient-to-t from-gray-700 to-gray-0 hover:cursor-pointer" onMouseEnter={() => setToggle(i)} onMouseLeave={() => setToggle(null)}> 
-           <img src={`/static/${x[2]}`} alt="" width={'184px'} height={'284px'} className="hover:mix-blend-overlay"/> 
-           <div className={`flex flex-col absolute ml-4 pointer-events-none ${toggle == i ? 'visible' : 'hidden'}`}> 
-               <div className="flex flex-col bg-primary-main justify-center text-center text-white w-28 h-8 mb-1"> 
-                 <Typo variant="body-semibold" className="text-sm"> {toUpper(x[1])} </Typo>    
-               </div>
-               <Typo variant="h3" className="text-white"> {x[0]} </Typo> 
-           </div>
-           </div>
-           </div>
-        })}
-        </div>
+        <>
+            <div className="flex flex-row justify-start w-full">
+                <Typo variant="h3"> Удирдлагууд </Typo>
+                <div className="flex flex-row justify-end w-full">
+                    <CustomButton variant="text" classname={def ? `text-white hover:text-white bg-primary-main hover:bg-primary-main border-2` : 'text-black hover:text-white border-2  bg-[#F7F7F7] hover:bg-primary-main'} onClick={option}> 2021-2022 он </CustomButton>
+                    <CustomButton variant="text" classname={def1 ? `text-white hover:text-white bg-primary-main hover:bg-primary-main border-2` : 'text-black hover:text-white border-2  bg-[#F7F7F7] hover:bg-primary-main'} onClick={option1}> 2019-2020 он </CustomButton>
+                    <CustomButton variant="text" classname={def2 ? `text-white hover:text-white bg-primary-main hover:bg-primary-main border-2` : 'text-black hover:text-white border-2  bg-[#F7F7F7] hover:bg-primary-main'} onClick={option2}> 2018-2019 он </CustomButton>
+                </div>
+            </div>
+            <div className="grid grid-rows-2 grid-flow-col gap-6 justify-start my-[36px]">
+                {data.map((x: any, i: number) => {
+                    return <div key={i}>
+                        <div className="flex flex-col justify-end w-full bg-gradient-to-t from-gray-700 to-gray-0 hover:cursor-pointer" onMouseEnter={() => setToggle(i)} onMouseLeave={() => setToggle(null)}>
+                            <img src={`/static/${x[2]}`} alt="" width={'184px'} height={'284px'} className="hover:mix-blend-overlay" />
+                            <div className={`flex flex-col absolute ml-4 pointer-events-none ${toggle == i ? 'visible' : 'hidden'}`}>
+                                <div className="flex flex-col bg-primary-main justify-center text-center text-white w-28 h-8 mb-1">
+                                    <Typo variant="body-semibold" className="text-sm"> {toUpper(x[1])} </Typo>
+                                </div>
+                                <Typo variant="h3" className="text-white"> {x[0]} </Typo>
+                            </div>
+                        </div>
+                    </div>
+                })}
+            </div>
         </>
     )
 };
--- src/context/cont.js
+++ src/context/cont.js
@@ -3,13 +3,30 @@ import { createContext, useContext, useState } from 'react'
 const AppContext = createContext()
 
 export function AppWrapper({ children }) {
+  const [link, setLink] = useState('/')
+  const [phone, setPhone] = useState(false)
   const [data, setData] = useState(false)
   const [data1, setData1] = useState(false)
   const [data2, setData2] = useState(false)
   const [data3, setData3] = useState(false)
   const [data4, setData4] = useState(false)
 
-  let sharedState = { data, setData, data1, setData1, data2, setData2, data3, setData3, data4, setData4 }
+  let sharedState = {
+    link,
+    setLink,
+    phone,
+    setPhone,
+    data,
+    setData,
+    data1,
+    setData1,
+    data2,
+    setData2,
+    data3,
+    setData3,
+    data4,
+    setData4,
+  }
 
   return <AppContext.Provider value={sharedState}>{children}</AppContext.Provider>
 }
